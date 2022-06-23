c9dea288dd14e76c9d7cfa2d32696024209f9f16
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-06-23 22:48:26 +0400

    TEST: get instagram post V4

--- front/src/Components/pages/Home/MainSliderPost.js
+++ front/src/Components/pages/Home/MainSliderPost.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Slider from "react-slick";
 import NewNextArrow from "../../common/NewNextArrow";
 import NewPrevArrow from "../../common/NewPrevArrow";
@@ -8,7 +8,7 @@ import MiniSliderPost from "./MiniSliderPost";
 
 const MainSliderPost = ({posts}) => {
     const [activeSlide, setActiveSlide] = useState(0)
-    const [loading, setLoading] = useState()
+    const [loading, setLoading] = useState(true)
 
     let settings = {
         dots: false,
@@ -22,56 +22,44 @@ const MainSliderPost = ({posts}) => {
         afterChange: current => setActiveSlide(current)
     }
 
-    const images = [{
-        id: "1", caption: "Подпись 1", like_count: 68, username: "Makson", media_url: img, children: {
-            data: [{media_url: img, id: "1a"}, {media_url: img2, id: "2a"}]
-        }, timestamp: "123"
-    }, {
-        id: "2", caption: "Подпись 2", like_count: 109, username: "Makson", media_url: img, timestamp: "123"
-    }, {
-        id: "3", caption: "Подпись 3", like_count: 209, username: "Makson", media_url: img, timestamp: "123"
-    }, {
-        id: "4", caption: "Подпись 4", like_count: 1234, username: "Makson", media_url: img, children: {
-            data: [{media_url: img, id: "1a"}, {media_url: img2, id: "2a"}]
-        }, timestamp: "123"
-    }, {
-        id: "5", caption: "Подпись 5", like_count: 3452, username: "Makson", media_url: img, children: {
-            data: [{media_url: img, id: "1a"}, {media_url: img2, id: "2a"}, {
-                media_url: img, id: "1a"
-            }, {media_url: img2, id: "2a"}, {media_url: img, id: "1a"}, {media_url: img2, id: "2a"}, {
-                media_url: img, id: "1a"
-            }, {media_url: img2, id: "2a"}, {media_url: img, id: "1a"}, {media_url: img2, id: "2a"}]
-        }, timestamp: "123"
-    },]
+    useEffect(() => {
+        if (posts.length) {
+                setLoading(false)
+        }
+    }, [posts])
 
     if (!posts.length) {
-        return (<span style={{textAlign: 'center', display: "block"}}>Постов нет</span>)
+        if (loading) {
+            return <span style={{textAlign: 'center', display: "block"}}>Загрузка...</span>
+        }
+        return <span style={{textAlign: 'center', display: "block"}}>Постов нет</span>
     } else {
-        return (<>
-            <Slider {...settings}>
-                {posts.map(post => (<div className="slider-post__item" key={post.id}>
-                    <div className="slider-post__item-top">
-                        <div className="post-slider__item-top_avatar">
-                            <p>{post.username}</p>
+        return (
+            <>
+                <Slider {...settings}>
+                    {posts.map(post => (<div className="slider-post__item" key={post.id}>
+                        <div className="slider-post__item-top">
+                            <div className="post-slider__item-top_avatar">
+                                <p>{post.username}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="slider-post__item-body">
-                        {post.children ? <MiniSliderPost images={post.children.data}/> :
-                            <div style={{backgroundImage: `url(${post.media_url})`}}
-                                 className="slider-post__item-body_img"/>}
-                    </div>
-                    <div className="slider-post__item-bottom flex-column">
+                        <div className="slider-post__item-body">
+                            {post.children ? <MiniSliderPost images={post.children.data}/> :
+                                <div style={{backgroundImage: `url(${post.media_url})`}}
+                                     className="slider-post__item-body_img"/>}
+                        </div>
+                        <div className="slider-post__item-bottom flex-column">
                             <span className="post-slider__item-like">
                                 <strong>Нравится: {post.like_count}</strong>
                             </span>
-                        <span className="post-slider__item-caption ">
+                            <span className="post-slider__item-caption ">
                                 <strong>{post.username}</strong> {post.caption}
                             </span>
-                    </div>
-                </div>))}
-            </Slider>
-            <p>{activeSlide}</p>
-        </>)
+                        </div>
+                    </div>))}
+                </Slider>
+                <p>{activeSlide}</p>
+            </>)
     }
 }
 
