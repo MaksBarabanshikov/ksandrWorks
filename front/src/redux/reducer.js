0124f0b1a97fc34da6e4e589b6e898638c0376e4
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-06-25 16:31:17 +0400

    connect redux logger

---
+++ front/src/redux/reducer.js
@@ -0,0 +1,5 @@
+import instaPostsReducer from "./modules/instaPostsSlice";
+
+export const reducer = {
+    instagramPosts: instaPostsReducer
+}
--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,33 +1,10 @@
-// import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
-// import thunk from 'redux-thunk'
-// import logger from 'redux-logger'
-//
-// import rootReducers from './modules/reducer'
-
-// const configureStore = (reducers = {}, preloadedState = {}, middlewares = []) => createStore(
-//     combineReducers({
-//         ...rootReducers,
-//         ...reducers
-//     }
-//     ),
-//     preloadedState,
-//     compose(
-//         applyMiddleware(
-//             ...middlewares,
-//             thunk,
-//             logger
-//         ),
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )
-// )
-//
-// export default configureStore()
-
 import { configureStore } from "@reduxjs/toolkit";
-import instaPostsReducer from "./modules/instaPostsSlice";
+import { createLogger } from "redux-logger"
+import { reducer } from "./reducer";
+
+const logger = createLogger()
 
 export default configureStore({
-    reducer: {
-        instagramPosts: instaPostsReducer
-    }
+    reducer: reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
