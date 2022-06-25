ea756a29686521c01c806cefd35f94093edf77a3
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-06-25 21:27:30 +0400

    test favorites v3

--- front/src/redux/modules/favoritesSlice.js
+++ front/src/redux/modules/favoritesSlice.js
@@ -19,10 +19,13 @@ export const uniqueId = () => (
     Math.random().toString(16).slice(2)
 )
 
+const file = localStorage.getItem('fileText')
+
 const favoritesSlice = createSlice({
     name: 'favorites',
     initialState: {
         favorites: [],
+        selectAllBtn: false,
         status: null,
         error: null
     },
@@ -41,15 +44,22 @@ const favoritesSlice = createSlice({
         }
     },
     extraReducers: {
-        [getFavoritesAPI.pending]: (state) => {
+        [getFavoritesAPI.pending]: state => {
             state.status = 'loading'
             state.error = null
         },
         [getFavoritesAPI.fulfilled]: (state, action) => {
             state.status = 'resolve'
-            state.favorites = action.payload
+            const textToArray = action.payload.filter(x => x !== "").map(x => "#" + x)
+            const size = 25; //размер подмассива
+            const subarray = []; //массив в который будет выведен результат.
+            for (let i = 0; i < Math.ceil(textToArray.length / size); i++) {
+                subarray[i] = textToArray.slice((i * size), (i * size) + size);
+            }
+
+            return state.favorites = subarray
         },
-        [getFavoritesAPI.rejected]: (state) => {
+        [getFavoritesAPI.rejected]: state => {
             state.status = 'error'
         },
     }
