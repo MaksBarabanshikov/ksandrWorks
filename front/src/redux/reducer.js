faad630331412c681ef3aa8d8b13eb7b5f5e69e7
MaksimBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-06-29 18:51:59 +0400

    refactor

--- front/src/redux/reducer.js
+++ front/src/redux/reducer.js
@@ -3,14 +3,12 @@ import instaPostsReducer from "./modules/instaPostsSlice";
 import favoritesReducer from "./modules/favoritesSlice";
 import modalReducer from "./modules/modalSlice";
 import facebookReducer from "./modules/facebookSlice";
-import {postsApi} from "./services/postsApi";
 import {hashtagsApi} from "./services/hashtagsApi";
 
 
 
 export const rootReducer = combineReducers({
     [hashtagsApi.reducerPath]: hashtagsApi.reducer,
-    [postsApi.reducerPath]: postsApi.reducer,
     modalFb: modalReducer,
     instagramPosts: instaPostsReducer,
     favorites: favoritesReducer,
